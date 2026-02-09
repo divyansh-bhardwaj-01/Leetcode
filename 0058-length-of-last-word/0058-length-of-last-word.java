1054d388e4f09e8712f9f5495f0b06dd973db3cf
Divyansh Bhardwaj <divyanshb240@gmail.com>
2026-02-09 19:12:15 +0530

    Time: 2 ms (10.67%), Space: 43.1 MB (67.79%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();    
+int count=0;
+        for (int i = 0; i < s.length(); i++) {
+            
+            if (s.charAt(i) == ' ') {
+                count = 0;    
+            } 
+            else {
+                count++;     
+            }
+        }
+        return count;
+    }
+}
