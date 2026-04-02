8da4f52173de40df50f78429e6202c493fd83938
Divyansh Bhardwaj <divyanshb240@gmail.com>
2026-04-02 17:30:13 +0530

    Time: 0 ms (100%), Space: 9 MB (30.97%) - LeetHub

---
+++ 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int firstMatchingIndex(string s) {
+        for(int i=0;i<s.length();i++){
+            if(s[i]==s[s.length()-1-i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
