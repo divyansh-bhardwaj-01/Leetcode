8cc35c258b251f17519d5e5a73e30a82620da6b0
Divyansh Bhardwaj <divyanshb240@gmail.com>
2026-04-12 10:21:28 +0530

    Time: 4 ms (36.15%), Space: 29.1 MB (6.22%) - LeetHub

---
+++ 3866-first-unique-even-element/3866-first-unique-even-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstUniqueEven(vector<int>& nums) {
+        map<int,int>mp;
+        for(int i:nums){
+            mp[i]++;
+        }
+        for(int i:nums){
+            if(i%2==0 && mp[i]==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
