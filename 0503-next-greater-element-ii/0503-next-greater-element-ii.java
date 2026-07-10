3349051fea9343a1bd692c4b352f25d869e4d291
Divyansh Bhardwaj <divyanshb240@gmail.com>
2026-07-10 22:42:32 +0530

    Time: 30 ms (13.05%), Space: 47.5 MB (91.5%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,35 +1,30 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        ArrayList<Integer>list=new ArrayList<>();
+         int[] arr=new int[nums.length];
         for(int i=0;i<nums.length;i++){
             int count=0;
             if(i!=nums.length-1){
                 for(int j=i+1;j<nums.length;j++){
                     if(nums[j]>nums[i]){
-                        list.add(nums[j]);
+                        arr[i]=nums[j];
                         count++;
                         break;
                     }
-                    
                  }
             }
              if(count==0){
                 for(int j=0;j<i;j++){
                     if(nums[j]>nums[i]){
-                        list.add(nums[j]);
+                        arr[i]=nums[j];
                         count++;
                         break;
                     }
                 }
             }
             if(count==0){
-                list.add(-1);
+               arr[i]=-1;
             }
-        } 
-        int[] arr=new int[nums.length];
-        for(int i=0;i<list.size();i++){
-            arr[i]=list.get(i);
-        }                      
+        }                   
         return arr;             
     }
 }
