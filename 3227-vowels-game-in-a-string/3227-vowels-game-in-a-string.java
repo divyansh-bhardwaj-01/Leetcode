3aa26d75f9646567f0b7f5d1d48426c5a9c21d66
Divyansh Bhardwaj <divyanshb240@gmail.com>
2026-08-19 17:30:04 +0530

    Time: 7 ms (43.55%), Space: 47.8 MB (49.08%) - LeetHub

---
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean doesAliceWin(String s) {
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+                count++;
+            }
+        }
+        if(count==0){
+            return false;
+        }
+        return true;
+    }
+}
