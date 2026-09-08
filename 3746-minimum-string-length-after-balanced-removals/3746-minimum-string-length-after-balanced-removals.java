d714e370095fe4d2f76f42dc44d68cf09a903c38
Divyansh Bhardwaj <divyanshb240@gmail.com>
2026-09-09 04:51:57 +0530

    Time: 3 ms (98.72%), Space: 47.3 MB (42.4%) - LeetHub

---
+++ 3746-minimum-string-length-after-balanced-removals/3746-minimum-string-length-after-balanced-removals.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minLengthAfterRemovals(String s) {
+        int a=0;
+        int b=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='a') a++;
+            else b++;
+        }
+        if(a>b) return a-b;
+        return b-a;
+    }
+}
